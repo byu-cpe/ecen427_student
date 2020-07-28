34fc15ae063fbf0e69555dbec9423599781df196
Jeff Goeders <jeff.goeders@gmail.com>
2020-07-28 07:37:19 +0000

    Add audio_codec and test files

---
+++ kernel/lab4_audio_codec/audio_codec.c
@@ -0,0 +1,88 @@
+#include <linux/cdev.h>
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your name");
+MODULE_DESCRIPTION("ECEn 427 Audio Driver");
+
+#define MODULE_NAME "audio"
+
+// Function declarations
+static int audio_init(void);
+static void audio_exit(void);
+
+module_init(audio_init);
+module_exit(audio_exit);
+
+struct audio_device {
+  int minor_num;                // Device minor number
+  struct cdev cdev;             // Character device structure
+  struct platform_device *pdev; // Platform device pointer
+  struct device *dev;           // device (/dev)
+
+  phys_addr_t phys_addr; // Physical address
+  u32 mem_size;          // Allocated mem space size
+  u32 *virt_addr;        // Virtual address
+
+  // Add any items to this that you need
+};
+
+// This is called when Linux loads your driver
+static int audio_init(void) {
+  pr_info("%s: Initializing Audio Driver!\n", MODULE_NAME);
+
+  // Get a major number for the driver -- alloc_chrdev_region; // pg. 45, LDD3.
+
+  // Create a device class. -- class_create()
+
+  // Register the driver as a platform driver -- platform_driver_register
+
+  // If any of the above functions fail, return an appropriate linux error code,
+  // and make sure
+  // you reverse any function calls that were successful.
+
+  return 0; //(Success)
+}
+
+// This is called when Linux unloads your driver
+static void audio_exit(void) {
+  pr_info("%s: Removing Audio Driver!\n", MODULE_NAME);
+  // platform_driver_unregister
+  // class_unregister and class_destroy
+  // unregister_chrdev_region
+  return;
+}
+
+// Called by kernel when a platform device is detected that matches the
+// 'compatible' name of this driver.
+static int audio_probe(struct platform_device *pdev) {
+  // Initialize the character device structure (cdev_init)
+  // Register the character device with Linux (cdev_add)
+
+  // Create a device file in /dev so that the character device can be accessed
+  // from user space (device_create).
+
+  // Get the physical device address from the device tree --
+  // platform_get_resource
+  // Reserve the memory region -- request_mem_region
+  // Get a (virtual memory) pointer to the device -- ioremap
+
+  // Get the IRQ number from the device tree -- platform_get_resource
+  // Register your interrupt service routine -- request_irq
+
+  // If any of the above functions fail, return an appropriate linux error code,
+  // and make sure
+  // you reverse any function calls that were successful.
+
+  return 0; //(success)
+}
+
+static int audio_remove(struct platform_device *pdev) {
+
+  // iounmap
+  // release_mem_region
+  // device_destroy
+  // cdev_del
+  return 0;
+}
---
+++ kernel/lab4_audio_codec/test_insmod.sh
@@ -0,0 +1,5 @@
+sudo insmod audio_codec.ko
+sudo rmmod audio_codec
+sudo insmod audio_codec.ko
+sudo rmmod audio_codec
+dmesg -T | tail -n 20
