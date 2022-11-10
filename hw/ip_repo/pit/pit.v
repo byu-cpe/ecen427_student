0fa05442b535e1fe4347682949d9a3de7dfc66ce
Jeff Goeders <jeff.goeders@gmail.com>
2022-11-09 19:31:39 -0700

    Fix missing axi connections
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- hw/ip_repo/pit/pit.v
+++ hw/ip_repo/pit/pit.v
@@ -47,21 +47,31 @@ module pit #(
     pit_sv_inst (    
     .s_axi_aclk(s_axi_aclk),
     .s_axi_aresetn(s_axi_aresetn),
+    
     .s_axi_awvalid(s_axi_awvalid),
     .s_axi_awready(s_axi_awready),
     .s_axi_awaddr(s_axi_awaddr),
+    .s_axi_awprot(s_axi_awprot),
+    
     .s_axi_wvalid(s_axi_wvalid),
     .s_axi_wready(s_axi_wready),
     .s_axi_wdata(s_axi_wdata),
+    .s_axi_wstrb(s_axi_wstrb,)
+
     .s_axi_bvalid(s_axi_bvalid),
     .s_axi_bready(s_axi_bready),
+    .s_axi_bresp(s_axi_bresp),
+
     .s_axi_arvalid(s_axi_arvalid),
     .s_axi_arready(s_axi_arready),
     .s_axi_araddr(s_axi_araddr),
+    .s_axi_arprot(s_axi_arprot),
+
     .s_axi_rdata(s_axi_rdata),
     .s_axi_rresp(s_axi_rresp),
     .s_axi_rvalid(s_axi_rvalid),
     .s_axi_rready(s_axi_rready),
+    
     .irq(irq)
 	);
 
