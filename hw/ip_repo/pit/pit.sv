d4ff0874db9767714fe392f21dd0af4bb1006c57
Jeff Goeders <jeff.goeders@gmail.com>
2024-12-16 17:16:53 -0700

    Update PIT default_nettype

--- hw/ip_repo/pit/pit.sv
+++ hw/ip_repo/pit/pit.sv
@@ -6,39 +6,39 @@ module pit_sv #(
     parameter integer C_S_AXI_ADDR_WIDTH = 4
 )
 (
-    input logic                             s_axi_aclk,
-    input logic                             s_axi_aresetn,
+    input wire logic                            s_axi_aclk,
+    input wire logic                            s_axi_aresetn,
 
     // AW
-    input logic                             s_axi_awvalid,
-    output logic                            s_axi_awready,
-    input logic [C_S_AXI_ADDR_WIDTH-1:0]    s_axi_awaddr,
-    input logic [2:0]                       s_axi_awprot,
+    input wire logic                            s_axi_awvalid,
+    output logic                                s_axi_awready,
+    input wire logic [C_S_AXI_ADDR_WIDTH-1:0]   s_axi_awaddr,
+    input wire logic [2:0]                      s_axi_awprot,
 
     // W
-    input logic                             s_axi_wvalid,
-    output logic                            s_axi_wready,
-    input logic [31:0]                      s_axi_wdata,
-    input logic [3:0]                       s_axi_wstrb,
+    input wire logic                            s_axi_wvalid,
+    output logic                                s_axi_wready,
+    input wire logic [31:0]                     s_axi_wdata,
+    input wire logic [3:0]                      s_axi_wstrb,
 
     // B
-    output logic                            s_axi_bvalid,
-    input logic                             s_axi_bready,
-    output logic [1:0]                      s_axi_bresp,
+    output logic                                s_axi_bvalid,
+    input wire  logic                           s_axi_bready,
+    output logic [1:0]                          s_axi_bresp,
 
     // AR
-    input logic                             s_axi_arvalid,
-    output logic                            s_axi_arready,
-    input logic [C_S_AXI_ADDR_WIDTH-1:0]    s_axi_araddr,
-    input logic [2:0]                       s_axi_arprot,
+    input wire logic                            s_axi_arvalid,
+    output logic                                s_axi_arready,
+    input wire logic [C_S_AXI_ADDR_WIDTH-1:0]   s_axi_araddr,
+    input wire logic [2:0]                      s_axi_arprot,
 
     // R
-    output logic [31:0]                     s_axi_rdata,
-    output logic [1:0]                      s_axi_rresp,
-    output logic                            s_axi_rvalid,
-    input logic                             s_axi_rready,
+    output logic [31:0]                         s_axi_rdata,
+    output logic [1:0]                          s_axi_rresp,
+    output logic                                s_axi_rvalid,
+    input wire logic                            s_axi_rready,
 
-    output logic                            irq
+    output logic                                irq
 );
 
 // Add your module here
