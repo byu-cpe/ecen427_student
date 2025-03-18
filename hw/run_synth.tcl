fcf79582f1fed366f9476919c5c8df34f14ed17c
Jeff Goeders <jeff.goeders@gmail.com>
2025-03-18 08:43:30 -0600

    Add run_synth.tcl

---
+++ hw/run_synth.tcl
@@ -0,0 +1,13 @@
+set_msg_config -new_severity "ERROR" -id "Synth 8-87"
+set_msg_config -new_severity "ERROR" -id "Synth 8-327"
+set_msg_config -new_severity "ERROR" -id "Synth 8-3352"
+set_msg_config -new_severity "ERROR" -id "Synth 8-5559"
+set_msg_config -new_severity "ERROR" -id "Synth 8-6090"
+set_msg_config -new_severity "ERROR" -id "Synth 8-6858"
+set_msg_config -new_severity "ERROR" -id "Synth 8-6859"
+set_msg_config -new_severity "ERROR" -id "Timing 38-282"
+set_msg_config -new_severity "ERROR" -id "VRFC 10-3091"
+set_msg_config -new_severity "WARNING" -id "Timing 38-313"
+set_msg_config -suppress -id "Constraints 18-5210"
+
+launch_runs synth_1 -jobs 4
