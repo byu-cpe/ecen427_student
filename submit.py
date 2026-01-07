ec970c92c0b6fe3cd8e84f0dc0967244a7eb1200
Jeff Goeders <jeff.goeders@gmail.com>
2026-01-07 13:58:35 -0700

    Add submit script

---
+++ submit.py
@@ -0,0 +1,43 @@
+import argparse
+import pathlib
+import sys
+
+import git
+
+ROOT_PATH = pathlib.Path(__file__).parent.resolve()
+
+sys.path.insert(0, str(ROOT_PATH / "py"))
+
+from test_suite_427 import TestSuite427
+
+
+choices = [
+    "lab1",
+    "lab2",
+    "lab3",
+    "lab4_m1",
+    "lab4_m2",
+    "lab4_m3",
+    "lab5_m1",
+    "lab5_m2",
+    "lab5_m3",
+    "lab5_m4",
+    "lab6_m1",
+    "lab6_m2",
+    "lab7",
+    "lab8",
+]
+
+parser = argparse.ArgumentParser(description="Submit lab assignments")
+parser.add_argument("lab", choices=choices, help="The lab to submit")
+args = parser.parse_args()
+
+
+repo = git.Repo(ROOT_PATH)
+
+test_suite = TestSuite427(
+    repo, lab_name=args.lab + "_submission", submit=True, max_repo_files=None
+)
+
+
+test_suite.run_tests()
