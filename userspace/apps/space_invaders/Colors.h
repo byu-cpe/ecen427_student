748c259d7e79e15cd8b6f9457df97b82621b7a9c
Jeff Goeders <jeff.goeders@gmail.com>
2026-01-28 14:40:58 -0700

    fix struct ordering

--- userspace/apps/space_invaders/Colors.h
+++ userspace/apps/space_invaders/Colors.h
@@ -5,22 +5,22 @@
 
 // An RGB pixel/color
 typedef struct {
-  uint8_t r;
   uint8_t g;
   uint8_t b;
+  uint8_t r;
 } rgb_t;
 
 // Constants for colors
 class Colors {
 public:
-  static const constexpr rgb_t BLACK = {0, 0, 0};
-  static const constexpr rgb_t WHITE = {0xFF, 0xFF, 0xFF};
-  static const constexpr rgb_t GREEN = {0, 0xFF, 0};
-  static const constexpr rgb_t RED = {0xFF, 0, 0};
-  static const constexpr rgb_t NAVY = {0, 46, 93};
-  static const constexpr rgb_t ROYAL = {0, 61, 165};
-  static const constexpr rgb_t PURPLE = {102, 45, 145};
-  static const constexpr rgb_t YELLOW = {255, 255, 0};
+  static const constexpr rgb_t BLACK = {.g = 0, .b = 0, .r = 0};
+  static const constexpr rgb_t WHITE = {.g = 0xFF, .b = 0xFF, .r = 0xFF};
+  static const constexpr rgb_t GREEN = {.g = 0xFF, .b = 0, .r = 0};
+  static const constexpr rgb_t RED = {.g = 0, .b = 0, .r = 0xFF};
+  static const constexpr rgb_t NAVY = {.g = 46, .b = 93, .r = 0};
+  static const constexpr rgb_t ROYAL = {.g = 61, .b = 165, .r = 0};
+  static const constexpr rgb_t PURPLE = {.g = 45, .b = 145, .r = 102};
+  static const constexpr rgb_t YELLOW = {.g = 255, .b = 0, .r = 255};
 };
 
 #endif /* APPS_SPACE_INVADERS_COLORS_H */
